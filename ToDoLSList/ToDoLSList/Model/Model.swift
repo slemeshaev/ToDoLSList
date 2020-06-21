5dd9fcca6182423abab4c24bf1744fa1d56efb8c
Stanislav <lemeshaevs@gmail.com>
2020-06-22 02:31:06 +0300

    Создал файлы для модели

---
+++ ToDoLSList/ToDoLSList/Model/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  ToDoLSList
+//
+//  Created by Станислав Лемешаев on 21.06.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
---
+++ ToDoLSList/ToDoLSList/Model/ToDoItem.swift
@@ -0,0 +1,9 @@
+//
+//  ToDoItem.swift
+//  ToDoLSList
+//
+//  Created by Станислав Лемешаев on 21.06.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
