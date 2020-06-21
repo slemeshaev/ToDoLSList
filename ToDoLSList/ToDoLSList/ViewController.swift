aa575f50559d8b927d63149f81cfa4d67433f100
Stanislav <lemeshaevs@gmail.com>
2020-06-21 16:07:08 +0300

    Создал файлы проекта

---
+++ ToDoLSList/ToDoLSList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoLSList
+//
+//  Created by Станислав Лемешаев on 21.06.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
