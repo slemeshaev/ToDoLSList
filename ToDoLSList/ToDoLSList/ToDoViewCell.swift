d07ab6339ce3b2c77a3e087f50228c9ecab39487
Stanislav <lemeshaevs@gmail.com>
2020-06-22 10:59:51 +0300

    Добавил файл для настройки ячейки

---
+++ ToDoLSList/ToDoLSList/ToDoViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoViewCell.swift
+//  ToDoLSList
+//
+//  Created by Станислав Лемешаев on 22.06.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import UIKit
+
+class ToDoViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
