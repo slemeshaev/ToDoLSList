65c5aade5eaf160e98d7ecca4bfa6875484c561f
Stanislav <lemeshaevs@gmail.com>
2020-06-24 19:17:54 +0300

    Добавил файл с протоклом

---
+++ ToDoLSList/ToDoLSList/Model/ToDoItemable.swift
@@ -0,0 +1,9 @@
+//
+//  ToDoItemable.swift
+//  ToDoLSList
+//
+//  Created by Станислав Лемешаев on 24.06.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
